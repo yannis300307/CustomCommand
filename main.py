3bf00493288dd1967b5620441b66f325398b9e2d
Yannis <52321382+yannis300307@users.noreply.github.com>
2024-10-02 11:38:48 +0200

    Mistake in exemple

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import customcommand
 
 
 @customcommand.command
-def test(a:float, b: str = 5, c: int = 8):
+def test(a:float, b: int = 5, c: str = "hello"):
     """A test function.
     That make things. But that things are very important in my program. Why?
     I don't know... If I remove it, it's not working the yeah, I will keep it."""
